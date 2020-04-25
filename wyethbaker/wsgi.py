103c0b758167227854ad26a0b6791d41ead4975b
Tom nyone <tom.nyone@gmail.com>
2020-04-25 18:24:21 +0100

    Made a little tweak

--- wyethbaker/wsgi.py
+++ wyethbaker/wsgi.py
@@ -5,4 +5,4 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wyethbaker.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
